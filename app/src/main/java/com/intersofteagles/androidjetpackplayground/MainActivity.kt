66ee5726941980d49e4468f5125a0603214efc81
Bedan Kamau <bedank5@gmail.com>
2018-12-05 17:33:35 +0300

    data binding

--- app/src/main/java/com/intersofteagles/androidjetpackplayground/MainActivity.kt
+++ app/src/main/java/com/intersofteagles/androidjetpackplayground/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.intersofteagles.androidjetpackplayground
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.intersofteagles.androidjetpackplayground.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding:ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.user = User("Jack", "Reacher", "Male")
+
     }
 }
---
+++ app/src/main/java/com/intersofteagles/androidjetpackplayground/User.kt
@@ -0,0 +1,7 @@
+package com.intersofteagles.androidjetpackplayground
+
+data class User(
+        val firstName:String,
+        val lastName:String,
+        val gender:String
+)
