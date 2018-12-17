2880c10d0aee382500d892921c15b01d5c6811ce
Bedan Kamau <bedank5@gmail.com>
2018-12-17 17:58:21 +0300

    added tests

--- app/src/test/java/com/intersofteagles/androidjetpackplayground/ExampleUnitTest.kt
+++ app/src/test/java/com/intersofteagles/androidjetpackplayground/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.intersofteagles.androidjetpackplayground
 import org.junit.Test
 
 import org.junit.Assert.*
+import com.intersofteagles.androidjetpackplayground.MainActivity
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +15,12 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+
+    @Test
+    fun test_canAddUp(){
+//        assertEquals(MainActivity)
+    }
+
+
 }
